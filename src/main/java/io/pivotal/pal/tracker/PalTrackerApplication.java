961d285fbb005bfc2e9813285e7b2454b77097cd
manoj07hbti <60manoj.kumar@gmail.com>
2018-07-11 11:53:31 +0800

    JDBC Template CRUD

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -20,6 +22,12 @@ public class PalTrackerApplication {
     TimeEntryRepository timeEntryRepository() {
         return new InMemoryTimeEntryRepository();
     }
+
+    @Bean
+    JdbcTimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
+    }
+
 
     @Bean
     public ObjectMapper jsonObjectMapper() {
